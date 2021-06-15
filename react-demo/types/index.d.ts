8f0d6e8f6bb9edf6bb39f67c592cf386f30cc184
JackYang <jack.yang@zoom.us>
2021-06-15 09:39:04 +0800

    remove others

--- react-demo/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import ZoomVideo from './zoomvideo';
-export * from './common';
-export * from './event-callback';
-export * from './media';
-export * from './chat';
-export * from './videoclient';
-export default ZoomVideo;
