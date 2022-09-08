7283f6b832dd1a4d38201890512341fde2870ad3
JackYang <jack.yang@zoom.us>
2022-09-08 10:45:30 +0800

    fix demo no role issue

--- react-demo/src/config/dev.ts
+++ react-demo/src/config/dev.ts
@@ -3,10 +3,12 @@ import { getExploreName } from '../utils/platform';
 export const devConfig = {
   sdkKey: '',
   sdkSecret: '',
+  webEndpoint: 'zoom.us',
   topic: '',
   name: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`,
   password: '',
   signature: '',
   sessionKey: '',
   userIdentity: '',
+  role: 1
 };
