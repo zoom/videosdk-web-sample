85a60ad4fa051f3aa7f23eed889f20962225c620
Mete Aykul <78444092+mayk-zoom@users.noreply.github.com>
2022-09-08 09:45:41 -0700

    Added clarifying comment around 'role' argument

--- react-demo/src/config/dev.ts
+++ react-demo/src/config/dev.ts
@@ -10,5 +10,6 @@ export const devConfig = {
   signature: '',
   sessionKey: '',
   userIdentity: '',
+  // role = 1 to join as host, 0 to join as attendee. The first user must join as host to start the session
   role: 1
 };
