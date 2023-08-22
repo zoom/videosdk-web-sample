99fd93d93bd49f02f00dc4468d0c7284fb9b062f
Jack.Yang <jack.yang@zoom.us>
2023-08-22 09:28:52 -0700

    update zoom video source

--- react-demo/src/feature/video/components/camera.tsx
+++ react-demo/src/feature/video/components/camera.tsx
@@ -20,10 +20,14 @@ interface CameraButtonProps {
   activeCamera?: string;
   activePlaybackUrl?: string;
 }
+
 const videoPlaybacks = [
-  { title: 'Video 1', url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4' },
-  { title: 'Video 2', url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4' },
-  { title: 'Video 3', url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4' }
+  { title: 'Video 1', url: 'https://source.zoom.us/brand/mp4/Using%20the%20Zoom%20PWA.mp4' },
+  { title: 'Video 2', url: 'https://source.zoom.us/brand/mp4/Zoom%20Cares%20Nonprofit%20Impact.mp4' },
+  {
+    title: 'Video 3',
+    url: 'https://source.zoom.us/brand/mp4/Zoom%20One%20-%20Team%20Chat%2C%20Phone%2C%20Email%2C%20and%20more.mp4'
+  }
 ];
 const CameraButton = (props: CameraButtonProps) => {
   const {
