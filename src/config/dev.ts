96ecd228a57584c0e0e79a7f3ee16db261e00a40
tommygaessler <tommy.gaessler@zoom.us>
2024-01-08 15:00:18 -0700

    added dev config file

---
+++ src/config/dev.ts
@@ -0,0 +1,15 @@
+import { getExploreName } from '../utils/platform';
+
+export const devConfig = {
+  sdkKey: '',
+  sdkSecret: '',
+  webEndpoint: 'zoom.us',
+  topic: '',
+  name: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`,
+  password: '',
+  signature: '',
+  sessionKey: '',
+  userIdentity: '',
+  // The user role. 1 to specify host or co-host. 0 to specify participant, Participants can join before the host. The session is started when the first user joins. Be sure to use a number type.
+  role: 1
+};
