fd79f5831b295fbca7f1ec20066fac4ad1bde1e8
tommygaessler <tommy.gaessler@zoom.us>
2024-01-08 13:09:07 -0700

    removed outdated sample app and flattened repo structure

--- API-Reference/readme.md
+++
@@ -1,1 +0,0 @@
-API reference https://marketplace.zoom.us/docs/sdk/video/web/reference
--- purejs-demo/src/js/meeting/session/video/video-render-props.js
+++
@@ -1,7 +0,0 @@
-export const VIDEO_CANVAS = document.getElementById('video-canvas');
-export const SELF_VIDEO_ELEMENT = document.getElementById('self-video');
-export const SELF_VIDEO_CANVAS = document.getElementById('self-video-canvas');
-export const VIDEO_CANVAS_DIMS = {
-    Width: 1280,
-    Height: 640,
-};
