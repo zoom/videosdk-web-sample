755050378fe07a960d9ab9f2b596fc5d274d7048
tommygaessler <tommy.gaessler@zoom.us>
2024-01-08 13:09:07 -0700

    removed outdated sample app and flattened repo structure

--- purejs-demo/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../css/index.css';
-import initPreviewButtons from './meeting/preview/preview';
-
-window.addEventListener('DOMContentLoaded', async () => {
-  console.log('======= Initializing preview =======');
-  await initPreviewButtons();
-  console.log('======= Done initializing preview =======');
-});
--- purejs-demo/src/js/meeting/preview/preview-video-props.js
+++
@@ -1,6 +0,0 @@
-export const PREVIEW_VIDEO_ELEMENT = document.getElementById('js-preview-video');
-
-export const PREVIEW_VIDEO_DIMS = {
-    Width: 800,
-    Height: 450,
-};
